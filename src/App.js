40a87dac771adf4f2e8ab044d044229a077eefaa
suatcg <suat-can_24@hotmail.com>
2022-08-29 03:10:57 +0300

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import "./App.css";
@@ -8,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText} </RightButton>}
-        </ButtonGroup>
-        <DownArrow src="/images/down-arrow.svg" />
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText} </RightButton>}
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Fade>
       </Buttons>
     </Wrap>
   );
@@ -36,6 +41,7 @@ const Wrap = styled.div`
 `;
 
 const ItemText = styled.div`
+  z-index: -1;
   padding-top: 15vh;
   text-aling: center;
 `;
